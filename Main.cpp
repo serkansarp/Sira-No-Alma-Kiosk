e0f8bc60ac056aa70dbbfd3a0f02131505cc6ed9
serkansarp <serkansarp79@gmail.com>
2023-06-05 23:48:48 +0300

    Do-While üzerinde çalışılıyor, 3'lü doğrulama yapılıyor, Türkiye Cumhuriyeti ilk vatandaşı Mustafa Kemal ATATÜRK'ün TC Kimlik No'sundan küçük olmamalı, 1'den 10'a kadar toplamın mod10'unun son haneyi vermesi ve aynı anda 1-3-5-7-9 hane toplamlarının 7 ile çarpımlarından 2-4-6-8. hanelerin toplamının çıkarma işlem sonucunun mod10'unun 10. haneyi vermesi gerekiyor. Üzerinde çalıştığım satırı --- ile işaretledim, devam edeceğim.

--- Main.cpp
+++ Main.cpp
@@ -17,32 +17,74 @@ int main() {
 	setlocale(LC_ALL, "Turkish");
 
 	long long int tcKimlik = 0;
+	int digitToplam = 0;
+	int tcKimlikDogru = 0;
+	int digit01 = 0, digit02 = 0, digit03 = 0, digit04 = 0, digit05 = 0, digit06 = 0;
+	int digit07 = 0, digit08 = 0, digit09 = 0, digit10 = 0, digit11 = 0, digit1to10Toplam = 0;
+	int digit10Dogrulama = 0;
 
 	cout << "\n\n   ---- NÖROLOJÝ / KARDÝYOLOJÝ / ONKOLOJÝ ----\n\n";
+	
+	do {
+		cout << "tc kimlik ver: ";
+		cin >> tcKimlik;
 
+		digit01 = (tcKimlik / 10000000000) % 10;
+		digit02 = (tcKimlik / 1000000000) % 10;
+		digit03 = (tcKimlik / 100000000) % 10;
+		digit04 = (tcKimlik / 10000000) % 10;
+		digit05 = (tcKimlik / 1000000) % 10;
+		digit06 = (tcKimlik / 100000) % 10;
+		digit07 = (tcKimlik / 10000) % 10;
+		digit08 = (tcKimlik / 1000) % 10;
+		digit09 = (tcKimlik / 100) % 10;
+		digit10 = (tcKimlik / 10) % 10;
+		digit11 = tcKimlik % 10;
+		digit1to10Toplam = digit01 + digit02 + digit03 + digit04 + digit05 + digit06 + digit07 + digit08 + digit09 + digit10;
+		//digit10Dogrulama = (((digit01 + digit03 + digit05 + digit07 + digit09) * 7) - (digit02 + digit04 + digit06 + digit08)) % 10;
 
-	cout << " Lütfen 11 haneli TC Kimlik Numaranýzý Giriniz: ";
-	cin >> tcKimlik;
+		cout << tcKimlik;
 
-	int digit01 = (tcKimlik / 10000000000) % 10;
-	int digit02 = (tcKimlik / 1000000000) % 10;
-	int digit03 = (tcKimlik / 100000000) % 10;
-	int digit04 = (tcKimlik / 10000000) % 10;
-	int digit05 = (tcKimlik / 1000000) % 10;
-	int digit06 = (tcKimlik / 100000) % 10;
-	int digit07 = (tcKimlik / 10000) % 10;
-	int digit08 = (tcKimlik / 1000) % 10;
-	int digit09 = (tcKimlik / 100) % 10;
-	int digit10 = (tcKimlik / 10) % 10;
-	int digit11 = tcKimlik % 10;
-	int digit1to10toplam = digit01 + digit02 + digit03 + digit04 + digit05 + digit06 + digit07 + digit08 + digit09 + digit10;
+		cout << tcKimlik < 10000000146;
 
-	if (digit1to10toplam % 10 != digit11 || tcKimlik < 1 || tcKimlik < 10000000012 || (((digit01 + digit03 + digit05 + digit07 + digit09) * 7) - (digit02 + digit04 + digit06 + digit08)) % 10 != digit10) {
-		cout << " \n TC Kimlik No hatalý girilmiþtir.\n Lütfen kontrol ederek tekrar giriniz." << endl;;
-	}
-	else {
-		cout << " iþleme devam" << endl;
-	}
+		if (digit1to10Toplam%10 == digit11) {
+			cout << "doðru";
+			tcKimlikDogru++;
+		}
+				
+		
+	}while (tcKimlikDogru != 1);
+	
+	/*
+	do {
+		cout << " Lütfen 11 haneli TC Kimlik Numaranýzý Giriniz: ";
+		cin >> tcKimlik;
+
+		digit01 = (tcKimlik / 10000000000) % 10;
+		digit02 = (tcKimlik / 1000000000) % 10;
+		digit03 = (tcKimlik / 100000000) % 10;
+		digit04 = (tcKimlik / 10000000) % 10;
+		digit05 = (tcKimlik / 1000000) % 10;
+		digit06 = (tcKimlik / 100000) % 10;
+		digit07 = (tcKimlik / 10000) % 10;
+		digit08 = (tcKimlik / 1000) % 10;
+		digit09 = (tcKimlik / 100) % 10;
+		digit10 = (tcKimlik / 10) % 10;
+		digit11 = tcKimlik % 10;
+		digitToplam = digit01 + digit02 + digit03 + digit04 + digit05 + digit06 + digit07 + digit08 + digit09 + digit10;
+
+		if (digit1to10toplam % 10 != digit11 || tcKimlik < 10000000146 || (((digit01 + digit03 + digit05 + digit07 + digit09) * 7) - (digit02 + digit04 + digit06 + digit08)) % 10 != digit10) {
+			cout << " \n TC Kimlik No hatalý girilmiþtir.\n Lütfen kontrol ederek tekrar giriniz." << endl << endl;
+		}
+		else {
+			tcKimlikDogru++;
+		}
+	} while (tcKimlikDogru == 0);
+
+	cout << " Devam";
+	*/
+	/*
+	
 
 	//cout << ;
 
