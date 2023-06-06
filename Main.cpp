1fdf76ef90b31e4229e18a1b1a02d8e11faec275
serkansarp <serkansarp79@gmail.com>
2023-06-06 14:38:41 +0300

    • Switch-Case ile devam edecek... • Girdinin int olup olmaması kontrolü eklendi • do-while'daki TC Kimlik No kontrolüne  mümkün olan en büyük TC Kimlik No'dan daha büyük bir sayı girilip girilmediği kontrolü eklendi

--- Main.cpp
+++ Main.cpp
@@ -5,11 +5,10 @@ using namespace std;
 /*
 
 // Sýra No Alma Kiosku
-
-- Tc Kimlik No Doðrulama
-- - Sayý hanesi bulma (stoi dýþýnda)
++ Tc Kimlik No Doðrulama
++ Sayý hanesi bulma (stoi dýþýnda)
 - Switch Case
-- Do-While Pratikleri
++ Do-While Pratikleri
 */
 
 
@@ -26,9 +25,19 @@ int main() {
 	cout << "\n\n   ---- NÖROLOJÝ / KARDÝYOLOJÝ / ONKOLOJÝ ----\n\n";
 	
 	do {
-		cout << "tc kimlik ver: ";
+		cout << " TC kimlik ver: ";
 		cin >> tcKimlik;
 
+		// Girdinin hatalý olmasý durumu
+		if (cin.fail()) {
+			cout << " Lütfen girdinizi sadece rakamlarý kullanarak yapýn." << endl << endl;
+			cin.clear();
+			cin.ignore();
+			tcKimlik = 0;
+			continue;
+		}
+		
+		// Digitleri yerine ekleme
 		digit01 = (tcKimlik / 10000000000) % 10;
 		digit02 = (tcKimlik / 1000000000) % 10;
 		digit03 = (tcKimlik / 100000000) % 10;
@@ -41,68 +50,29 @@ int main() {
 		digit10 = (tcKimlik / 10) % 10;
 		digit11 = tcKimlik % 10;
 		digit1to10Toplam = digit01 + digit02 + digit03 + digit04 + digit05 + digit06 + digit07 + digit08 + digit09 + digit10;
-		//digit10Dogrulama = (((digit01 + digit03 + digit05 + digit07 + digit09) * 7) - (digit02 + digit04 + digit06 + digit08)) % 10;
+		digit10Dogrulama = (((digit01 + digit03 + digit05 + digit07 + digit09) * 7) - (digit02 + digit04 + digit06 + digit08)) % 10;
 
-		cout << tcKimlik;
-
-		cout << tcKimlik < 10000000146;
-
-		if (digit1to10Toplam%10 == digit11) {
-			cout << "doðru";
-			tcKimlikDogru++;
-		}
-				
-		
-	}while (tcKimlikDogru != 1);
-	
-	/*
-	do {
-		cout << " Lütfen 11 haneli TC Kimlik Numaranýzý Giriniz: ";
-		cin >> tcKimlik;
-
-		digit01 = (tcKimlik / 10000000000) % 10;
-		digit02 = (tcKimlik / 1000000000) % 10;
-		digit03 = (tcKimlik / 100000000) % 10;
-		digit04 = (tcKimlik / 10000000) % 10;
-		digit05 = (tcKimlik / 1000000) % 10;
-		digit06 = (tcKimlik / 100000) % 10;
-		digit07 = (tcKimlik / 10000) % 10;
-		digit08 = (tcKimlik / 1000) % 10;
-		digit09 = (tcKimlik / 100) % 10;
-		digit10 = (tcKimlik / 10) % 10;
-		digit11 = tcKimlik % 10;
-		digitToplam = digit01 + digit02 + digit03 + digit04 + digit05 + digit06 + digit07 + digit08 + digit09 + digit10;
-
-		if (digit1to10toplam % 10 != digit11 || tcKimlik < 10000000146 || (((digit01 + digit03 + digit05 + digit07 + digit09) * 7) - (digit02 + digit04 + digit06 + digit08)) % 10 != digit10) {
-			cout << " \n TC Kimlik No hatalý girilmiþtir.\n Lütfen kontrol ederek tekrar giriniz." << endl << endl;
+		/*
+		1. Digit11 doðrulama hatasý / 2. Ýlk TC Kimlik No'dan küçük olmasý kontrolü
+		3. 10. digit doðrulama / 4. Verilebilecek en büyük, önceki 3 yöntemi de doðrulayan TC Kimlik No'dan büyük olmamasý
+		þeklinde 4 kontrol altta yapýldýktan sonra programýn devamý saðlanýyor.
+		*/
+		if (digit1to10Toplam % 10 != digit11 || tcKimlik < 10000000146 || digit10Dogrulama!=digit10 || tcKimlik > 99999999990) {
+			cout << " Girdiðiniz TC Kimlik No hatalýdýr, lütfen kontrol ederek tekrar giriniz." << endl;
 		}
 		else {
 			tcKimlikDogru++;
 		}
-	} while (tcKimlikDogru == 0);
-
-	cout << " Devam";
-	*/
-	/*
+	} while (tcKimlikDogru != 1);
 	
-
-	//cout << ;
-
-	/*
-	T.C.Kimlik Numaralarýmýzýn 1. 3. 5. 7. ve 9. hanelerinin toplamýnýn 7 katýndan,
-	2. 4. 6. ve 8. hanelerinin toplamý çýkartýldýðýnda, elde edilen sonucun 10'a bölümünden kalan, yani Mod10'u bize 10. haneyi verir.
-
-		26*7=182-7=175
-		7
-
-	12345678901
-	40633024912
-	25211412244
+	
+	
+	cout << "devam";
 
 	//switch case
 	
 	
-	*/
+	
 	
 	
 	
